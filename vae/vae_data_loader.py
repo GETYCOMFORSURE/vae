01c93d170cb495b132c1299bf23f435b2faf5436
Siyun <siyun_cao@outlook.com>
2026-07-10 16:52:12 +0800

    remove unnecessary bits in vae_data_loader.py

--- vae/vae_data_loader.py
+++ vae/vae_data_loader.py
@@ -1,4 +1,3 @@
-# 1. data loader
 train_data = datasets.MNIST(
     root='./data', 
     train=True, 
@@ -11,21 +10,6 @@ test_data = datasets.MNIST(
     download=True, 
     transform=transforms.ToTensor())
 
-# take a glimpse:
-image, label = train_data[0]
-plt.imshow(image.squeeze(), cmap='gray')
-plt.title(f'Label: {label}')
-plt.show()
-
 # shuffle + batch
 train_loader = DataLoader(train_data, batch_size=32, shuffle=True) # returns a DataLoader object
 test_loader = DataLoader(test_data, batch_size=32, shuffle=False)
-
-# take a glimpse:
-images, labels = next(iter(train_loader)) 
-image = images[0]
-label = labels[0]
-
-plt.imshow(image.squeeze(), cmap='gray')
-plt.title(f'Label: {label}')
-plt.show()
